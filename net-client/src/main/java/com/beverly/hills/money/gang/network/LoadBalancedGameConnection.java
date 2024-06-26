7f6b76ff541feaf5e0d9b173ba4e6459e000c9ee
Beverly Hills Money Gangster <beverly.hills.money.gangster@gmail.com>
2024-06-26 18:27:10 +0300

    Save progress

---
+++ net-client/src/main/java/com/beverly/hills/money/gang/network/LoadBalancedGameConnection.java
@@ -0,0 +1,96 @@
+package com.beverly.hills.money.gang.network;
+
+import com.beverly.hills.money.gang.proto.GetServerInfoCommand;
+import com.beverly.hills.money.gang.proto.JoinGameCommand;
+import com.beverly.hills.money.gang.proto.PushChatEventCommand;
+import com.beverly.hills.money.gang.proto.PushGameEventCommand;
+import com.beverly.hills.money.gang.proto.RespawnCommand;
+import com.beverly.hills.money.gang.proto.ServerResponse;
+import com.beverly.hills.money.gang.stats.NetworkStatsReader;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.NonNull;
+
+
+public class LoadBalancedGameConnection {
+
+  private final GameConnection gameConnection;
+
+  @Getter
+  private final Iterable<SecondaryGameConnection> secondaryGameConnections;
+  private final List<AbstractGameConnection> allConnections = new ArrayList<>();
+
+  public LoadBalancedGameConnection(
+      final @NonNull GameConnection gameConnection,
+      final @NonNull List<SecondaryGameConnection> secondaryGameConnections) {
+    if (secondaryGameConnections.isEmpty()) {
+      throw new IllegalArgumentException(
+          "Need at least one secondary connection for load balancing");
+    }
+    this.secondaryGameConnections = secondaryGameConnections;
+    this.gameConnection = gameConnection;
+    this.allConnections.add(gameConnection);
+    this.allConnections.addAll(secondaryGameConnections);
+  }
+
+  public void write(PushGameEventCommand pushGameEventCommand) {
+    gameConnection.write(pushGameEventCommand);
+  }
+
+  public void write(RespawnCommand respawnCommand) {
+    gameConnection.write(respawnCommand);
+  }
+
+  public void write(PushChatEventCommand pushChatEventCommand) {
+    gameConnection.write(pushChatEventCommand);
+  }
+
+  public void write(JoinGameCommand joinGameCommand) {
+    gameConnection.write(joinGameCommand);
+  }
+
+  public void write(GetServerInfoCommand getServerInfoCommand) {
+    gameConnection.write(getServerInfoCommand);
+  }
+
+  public NetworkStatsReader getNetworkStats() {
+    return gameConnection.getNetworkStats();
+  }
+
+  public void disconnect() {
+    allConnections.forEach(AbstractGameConnection::disconnect);
+  }
+
+  public boolean isAllConnected() {
+    return allConnections.stream().allMatch(AbstractGameConnection::isConnected);
+  }
+
+  public boolean isAnyDisconnected() {
+    return !isAllConnected();
+  }
+
+  public List<Throwable> pollErrors() {
+    List<Throwable> polledErrors = new ArrayList<>();
+    allConnections.forEach(abstractGameConnection ->
+        polledErrors.addAll(abstractGameConnection.getErrors().poll(Integer.MAX_VALUE)));
+    return polledErrors;
+  }
+
+  public List<ServerResponse> pollResponses() {
+    List<ServerResponse> polledResponses = new ArrayList<>();
+    allConnections.forEach(abstractGameConnection ->
+        polledResponses.addAll(abstractGameConnection.getResponse().poll(Integer.MAX_VALUE)));
+    return polledResponses;
+  }
+
+  public boolean waitUntilAllConnected(int timeoutMls) throws InterruptedException {
+    for (AbstractGameConnection connection : allConnections) {
+      if (!connection.waitUntilConnected(timeoutMls)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+}
---
+++ net-client/src/main/java/com/beverly/hills/money/gang/network/SecondaryGameConnection.java
@@ -0,0 +1,17 @@
+package com.beverly.hills.money.gang.network;
+
+import com.beverly.hills.money.gang.entity.GameServerCreds;
+import com.beverly.hills.money.gang.proto.MergeConnectionCommand;
+import java.io.IOException;
+
+public class SecondaryGameConnection extends AbstractGameConnection {
+
+  public SecondaryGameConnection(GameServerCreds gameServerCreds) throws IOException {
+    super(gameServerCreds);
+  }
+
+  public void write(MergeConnectionCommand mergeConnectionCommand) {
+    writeLocal(mergeConnectionCommand);
+  }
+
+}
